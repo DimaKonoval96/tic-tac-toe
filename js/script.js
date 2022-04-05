a3a60b02f4d6a94f7ca632b8dab78200aa48750c
DimaKonoval96 <konoval6991@gmail.com>
2022-04-05 13:48:20 +0300

    Created 'gameBoard' and 'gameController' modules, and added displayBoard method that walk through board array and display items to the DOM

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
-console.log("Hello world!");
+// The game board module
+const gameBoard = (() => {
+  const board = [
+    ["", "", ""],
+    ["x", "o", "o"],
+    ["", "x", ""],
+  ];
+
+  const getBoard = () => {
+    return board;
+  };
+
+  return { getBoard };
+})();
+
+// The players factory
+const players = () => {
+  return {};
+};
+
+// The game flow controller module
+const gameController = (() => {
+  const dGameBoard = document.querySelector("#GameBoard");
+  const dCells = document.querySelectorAll(".cell");
+
+  const displayBoard = () => {
+    dCells.forEach((cell) => {
+      let row = cell.dataset.cell[0];
+      let column = cell.dataset.cell[1];
+      cell.textContent = gameBoard.getBoard()[row][column];
+    });
+  };
+  return { displayBoard };
+})();
+
+gameController.displayBoard();
