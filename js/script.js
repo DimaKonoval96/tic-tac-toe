21e7479b3c6ca418720c3832111a3f03cb532329
DimaKonoval96 <konoval6991@gmail.com>
2022-04-05 17:15:42 +0300

    Added player's turn functionality

--- js/script.js
+++ js/script.js
@@ -22,26 +22,33 @@ const gameBoard = (() => {
 })();
 
 // The players factory
-const players = () => {
-  return {};
+const players = (name, value) => {
+  return { name, value };
 };
 
 // Controller module of the game flow
 const gameController = (() => {
+  const player1 = players("Player 1", "x");
+  const player2 = players("Player 2", "o");
+
+  let currPlayer = player1;
   const dGameBoard = document.querySelector("#GameBoard");
   const dCells = document.querySelectorAll(".cell");
   dGameBoard.addEventListener("click", (ev) => {
     const index = ev.target.dataset.cell;
     console.log(index);
-    gameBoard.updateBoard(index, "x");
+    gameBoard.updateBoard(index, currPlayer.value);
+    currPlayer = currPlayer == player1 ? player2 : player1;
     displayBoard();
   });
+
   const displayBoard = () => {
     dCells.forEach((cell) => {
       let row = cell.dataset.cell[0];
       let column = cell.dataset.cell[1];
       cell.textContent = gameBoard.getBoard()[row][column];
     });
+    console.log(player1, player2);
   };
   return { displayBoard };
 })();
